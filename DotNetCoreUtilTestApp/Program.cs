fb705c5d19bade53fdf014fdf1f85ed67b797c7a
Daiyuu Nobori COE <dnobori.gitcommit@coe.ad.jp>
2018-07-24 16:10:50 +0900

    初期作成

---
+++ DotNetCoreUtilTestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DotNetCoreUtil;
+
+namespace DotNetCoreUtilTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Class1.Test();
+        }
+    }
+}
