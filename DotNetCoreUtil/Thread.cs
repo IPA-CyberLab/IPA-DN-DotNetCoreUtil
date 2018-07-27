a2f5779215cad72e3b32c1a10f3b6e9e7fd0b949
Daiyuu Nobori COE <dnobori.gitcommit@coe.ad.jp>
2018-07-27 09:16:02 +0900

    test

--- DotNetCoreUtil/Thread.cs
+++ DotNetCoreUtil/Thread.cs
@@ -98,7 +98,15 @@ namespace IPA.DN.CoreUtil
                 Permissions perm = Permissions.S_IRUSR | Permissions.S_IWUSR | Permissions.S_IRGRP | Permissions.S_IWGRP | Permissions.S_IROTH | Permissions.S_IWOTH;
 
                 IntPtr f = Open("/", OpenFlags.O_CREAT, (int)perm);
-                FLock(f, LockOperations.LOCK_EX);
+                if (f.ToInt64() < 0)
+                {
+                    throw new IOException("Open failed.");
+                }
+
+                if (FLock(f, LockOperations.LOCK_EX) == -1)
+                {
+                    throw new IOException("FLock failed.");
+                }
 
                 this.fs = f;
 
