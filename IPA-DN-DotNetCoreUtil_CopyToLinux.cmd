67d15d211ac89beab3e34cb2ebd34096031cde74
Daiyuu Nobori COE <dnobori.gitcommit@coe.ad.jp>
2018-07-28 09:02:00 +0900

    簡易 copy

---
+++ IPA-DN-DotNetCoreUtil_CopyToLinux.cmd
@@ -0,0 +1,2 @@
+c:\windows\system32\Robocopy.exe C:\git\IPA-DN-DotNetCoreUtil \\10.40.0.109\vmdata1\containers\dn-ubtest1\rootfs\root\IPA-DN-DotNetCoreUtil-Copy /E /COPY:DAT /IT /R:2 /W:1 /XD .git .vs Debug Release obj
+
